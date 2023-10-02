54dd49ef66a814d806882bf5817bbe3778a9625c
SuciuAlis <61494544+SuciuAlis@users.noreply.github.com>
2023-10-02 13:19:01 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,6 +34,7 @@ public class Main {
         anotherVulnerability2();
         anotherVulnerability6();
         anotherVulnerability7();
+        anotherVulnerability8();
         //PENTRU GUI
         // de avut un check: daca profesorul introduce sau nu saptamana la timp
         // daca se introduce nota la timp, se preia saptamana din sistem
@@ -81,6 +82,14 @@ public class Main {
         conn.setRequestProperty("Authorization", "Basic " + encoding); // Noncompliant
     }
     public static void anotherVulnerability7() throws IOException {
+        String encoding = Base64.getEncoder().encodeToString(("login:passwd").getBytes("UTF-8"));
+        URL url = new URL("www.center.com");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+        conn.setRequestProperty("Authorization", "Basic " + encoding); // Noncompliant
+    }
+    public static void anotherVulnerability8() throws IOException {
         String encoding = Base64.getEncoder().encodeToString(("login:passwd").getBytes("UTF-8"));
         URL url = new URL("www.center.com");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
